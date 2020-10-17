cd4670c937ddd7bd2f8fefcd25d66151663fbdf8
Raymond Coplin <raymondcoplin@gmail.com>
2020-10-16 22:21:34 -0400

    Updated app name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,12 +12,6 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     
-    const myObservable = Observable.create((producer) => {
-      setTimeout(() => producer.next('Emision 1'), 1000);
-      setTimeout(() => producer.next('Emision 2'), 2000);
-      setTimeout(() => producer.next('Emision 3'), 3000);
-    });
-
   }
 
 }
